73bf7af4fc69a1a711ea3d6989d517254b01c37e
Pradip Gangarde <pradipgangarde05@gmail.com>
2023-06-16 01:26:57 +0530

    todo

--- src/main.js
+++ src/main.js
@@ -6,42 +6,61 @@ const app = express();
 app.use(cors()); // allowing everyone.
 
 async function addrecord(req, res) {
+}
+
+async function findAllMessage(req, res) {
+}
+
+function helloPost(req, res) {
+  let result = { opr: true };
+  res.json(result);
+}
+
+// NEW TODO API
+async function addTodo(req, res) {
   const uri = "mongodb://127.0.0.1:27017";
   const client = new MongoClient(uri);
 
-  const db = client.db("mydb");
-  const messageColl = db.collection("message");
+  const db = client.db("project");
+  const messageColl = db.collection("todo");
 
   let inputDoc = {
-    message: req.query.message || "default",
+    task: req.query.task,
+    description: req.query.description,
   };
   await messageColl.insertOne(inputDoc);
 
   await client.close();
 
-  // string response
-  // res.send("record added")
-
-  // json response :: preferred
   res.json({ opr: "success" });
 }
 
-async function findAllMessage(req, res) {
+async function addUserRecord(req, res) {
   const uri = "mongodb://127.0.0.1:27017";
   const client = new MongoClient(uri);
 
-  const db = client.db("mydb");
-  const messageColl = db.collection("message");
+  const db = client.db("project");
+  const messageColl = db.collection("user");
 
-  let list = await messageColl.find().toArray();
+  let inputDoc = {
+    username: req.query.username,
+    password: req.query.password,
+    email: req.query.email,
+    mobile: req.query.mobile,
+  };
+  await messageColl.insertOne(inputDoc);
 
   await client.close();
-  res.json(list);
+
+  res.json({ opr: "success" });
 }
 
 // http://localhost:4000/addrecord
 app.get("/addrecord", addrecord);
 app.get("/findAll", findAllMessage);
+app.post("/hello", helloPost);
+app.get("/addtodo", addTodo);
+app.get("/adduser", addUserRecord);
 
 // http://localhost:4000/
 app.listen(4000);
